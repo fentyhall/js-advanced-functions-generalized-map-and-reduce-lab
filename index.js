494726afb972c657cd697f9391d799b4f5c6ea7f
fentyhall <fentymhall@gmail.com>
2021-02-15 20:08:36 -0800

    commit tree 254815b8ffb4799f047908eafc77e012501c1a51

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your functions here
+function map(arr, fn) {
+    return arr.map(element => fn(element));
+}
+
+function reduce(arr, fn, start) {
+    let result = (!!start) ? start : arr[0];
+    let i = (!!start) ? 0 : 1;
+    
+    for (; i < arr.length; i++) {
+        result = fn(arr[i], result);
+    }
+    
+    return result;
+}
